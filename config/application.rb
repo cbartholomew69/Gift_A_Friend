f49b847ecdf7813e3ba47b0705f588383b359826
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-07-08 17:44:57 -0400

    Initial Commit set up angular with devise on rails

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module GiftAFriend
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
