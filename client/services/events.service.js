0ac05d9f53c32348607a408a181682b573448957
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-07-09 21:38:48 -0400

    added event service.js, signup not working

---
+++ client/services/events.service.js
@@ -0,0 +1,22 @@
+eventsService.$inject = ["$http"];
+function eventsService($http){
+    const service = this;
+
+    service.getEvents = function(){
+        return $http.get("/events").then(res => res.date);
+    };
+
+    service.getEvent = function (id) {
+        return $http.get('/events/${id}').then(res => res.date);
+    };  
+          
+    service.saveEvent = function (newEvent) {
+        return $http.post("/events", newEvent).then(res => {
+           return res.date;
+        });
+    };          
+    
+    return service;
+}
+
+angular.module("myApp").service("eventsService", eventsService);
